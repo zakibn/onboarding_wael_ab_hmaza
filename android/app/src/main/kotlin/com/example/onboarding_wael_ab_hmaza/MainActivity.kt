68624c30f0beeb06108f9a7f1f3497abc253cad7
zakibn <zakariachafai77@gmail.com>
2024-03-09 08:33:37 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/onboarding_wael_ab_hmaza/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onboarding_wael_ab_hmaza
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'core/constant/routes.dart';
+import 'view/screen/auth/login.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  AppRoute.login: (context) => const Login()
+};
